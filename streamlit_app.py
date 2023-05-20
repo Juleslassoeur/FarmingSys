bbaad57184e9828a14fa7f41137cd91e5d10d562
Juleslassoeur <78729492+Juleslassoeur@users.noreply.github.com>
2023-05-20 13:31:04 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,16 +4,11 @@ import math
 import pandas as pd
 import streamlit as st
 
-"""
-# Welcome to Streamlit!
+csv_url = "https://raw.githubusercontent.com/Juleslassoeur/FarmingSys/main/df4.csv"
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+df = pd.read_csv(csv_url)
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+st.write(df)
 
 
 with st.echo(code_location='below'):
