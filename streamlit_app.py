d482d5c7561a693a41cb91979c2a61063a9e1459
Juleslassoeur <78729492+Juleslassoeur@users.noreply.github.com>
2023-05-20 13:33:19 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,11 +4,56 @@ import math
 import pandas as pd
 import streamlit as st
 
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+
+#données
+list_cereales = {
+    'Blé' : 1, 
+    'Orge' : 2, 
+    'Maïs' : 3, 
+    'Tournesol' : 4, 
+    'Colza' : 5, 
+    'Soja' : 6
+}
+
+list_produits = {
+    'Phyto A' : 1, 
+    'Phyto B' : 2, 
+    'Phyto C' : 3, 
+    'Phyto D' : 4, 
+    'Phyto E' : 5
+}
+
+list_maladies = {
+    'A': 1, 
+    'B': 2, 
+    'C': 3, 
+    'D': 4, 
+    'E': 5,
+    'F': 6
+}
+
 csv_url = "https://raw.githubusercontent.com/Juleslassoeur/FarmingSys/main/df4.csv"
 
 df = pd.read_csv(csv_url)
+df['Produit'] = df['Produit'].replace(list_produits)
+df['Maladie'] = df['Maladie'].replace(list_maladies)
 
-st.write(df)
+df_display = pd.read_csv(csv_url)
+
+array = df.values
+X = array[:,0:5]
+y = array[:,5:7]
+
+X_train, X_validation, Y_train, Y_validation = train_test_split(X, y, test_size=0.20, random_state=1)
+model = LinearRegression()
+model.fit(X, y)
+
+st.title("Modèle d'analyse des données agricoles")
+
+st.write("Aperçu du data set : ")
+st.write(df_display)
 
 
 with st.echo(code_location='below'):
